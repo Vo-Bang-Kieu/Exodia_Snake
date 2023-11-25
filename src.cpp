fb1109ab3ecdb769aa25c04e66eb7d781f1f816b
DUONG TRONG NHAN <23730023@ms.uit.edu.vn>
2023-11-25 23:40:06 +0700

    Create src.cpp

---
+++ src.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+int main(){
+    gotoxy(5,5);
+    cout<<"Hello, world!";
+}
