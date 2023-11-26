5d3763de0ee8b18bbe5db90ecb977c1c533af3ba
NguyenTranNgocNgoc <23730034@ms.uit.edu.vn>
2023-11-26 23:59:19 +0700

    Update src.cpp

--- src.cpp
+++ src.cpp
@@ -9,7 +9,28 @@ void gotoxy(int x, int y) {
     coord.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+void draw_frame(){
+    int row = 27;
+    int col = 77;
+    //27-2 = 25; 77 - 2 = 75
+    for(int j =0; j<col;j++){
+            cout<<"_";
+        }
+    cout<<endl;
+    for(int i = 1;i<row-1;i++){
+        for(int j =0; j<col;j++){
+            if(j==0||j==col-1)
+                cout<<"|";
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
+    for(int j = 0; j<col;j++){
+            cout<<"*";
+        }
+
+}
+
 int main(){
-    gotoxy(5,5);
-    cout<<"Hello, world!";
+    draw_frame();
 }
